3bf40aa965d6657515375c613e021c5541de95d3
Lucia Viazzo <viazzo94@gmail.com>
2024-03-23 15:57:09 -0300

    Clase contratado, versión inicial

---
+++ src/ar/edu/unq/po2/tp2/Contratado.java
@@ -0,0 +1,23 @@
+package ar.edu.unq.po2.tp2;
+
+import java.time.LocalDate;
+
+public class Contratado extends Empleado{
+	private int numeroContrato;
+	private String medioDePago;
+
+	public Contratado(String nombre, String direccion, String estadoCivil, LocalDate fechaNac, float suledoBasico,
+			int numeroContrato, String medioDePago) {
+		super(nombre, direccion, estadoCivil, fechaNac, suledoBasico);
+	}
+
+	@Override
+	public float calcularSueldoBruto() {
+		return sueldoBasico;
+	}
+
+	@Override
+	public float calcularRetenciones() {
+		return 50;
+	}
+}
