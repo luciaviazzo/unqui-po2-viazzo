9522ba874892e798a753381d5e040892defa65cd
Lucia Viazzo <viazzo94@gmail.com>
2024-03-23 17:15:08 -0300

    Clase reciboHaberes, version inicial

--- src/ar/edu/unq/po2/tp2/ReciboHaberes.java
+++ src/ar/edu/unq/po2/tp2/ReciboHaberes.java
@@ -1,5 +1,52 @@
 package ar.edu.unq.po2.tp2;
 
-public class ReciboHaberes {
+import java.util.Date;
 
+
+public class ReciboHaberes {
+	
+	public String nombre;
+	public String direccion;
+	public Date fechaEmision;
+	public float sueldoBruto;
+	public float sueldoNeto;
+	
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+	public void setFechaEmision(Date fechaEmision) {
+		this.fechaEmision = fechaEmision;
+	}
+	
+	
+	public void setSueldoBruto(float sueldoBruto) {
+		this.sueldoBruto = sueldoBruto;
+	}
+	
+
+	public void setSueldoNeto(float sueldoNeto) {
+		this.sueldoNeto = sueldoNeto;
+	}
+	
+	
+	public void setAll(Empleado empleado, Date fechaEmision) {
+		setNombre(empleado.getNombre());
+		setDireccion(empleado.getDireccion());
+		setSueldoBruto(empleado.calcularSueldoBruto());
+		setSueldoNeto(empleado.calcularSueldoNeto());
+		setFechaEmision(fechaEmision);
+	}
+
+
+	public void generarRecibo(Empleado empleado, Date fechaEmision) {
+		setAll(empleado, fechaEmision);
+	}
 }
