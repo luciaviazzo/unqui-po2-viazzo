53a192db1eda908ea28ac26075ce105ff459df85
Lucia Viazzo <viazzo94@gmail.com>
2024-03-23 15:29:43 -0300

    Clase empresa, versión inicial

---
+++ src/ar/edu/unq/po2/tp2/Empresa.java
@@ -0,0 +1,48 @@
+package ar.edu.unq.po2.tp2;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.function.Function;
+
+import tp2.Empleado;
+import tp2.ReciboHaberes;
+
+public class Empresa {
+	private String nombre;
+	private int CUIT;
+	private ArrayList<Empleado> empleados;
+
+	public Empresa(String nombre, int CUIT) {
+		this.nombre = nombre;
+		this.CUIT = CUIT;
+		this.empleados = new ArrayList<Empleado>();
+	}
+
+	public ArrayList<Empleado> getEmpleados() {
+		return empleados;
+	}
+
+	public void agregarEmpleado(Empleado empleado) {
+		empleados.add(empleado);
+	}
+
+	private float calcularTotal(Function<Empleado, Float> calcular) {
+		float total = 0;
+		for (Empleado empleado : empleados) {
+			total += calcular.apply(empleado);
+		}
+		return total;
+	}
+
+	public float calcularTotalRetenciones() {
+		return calcularTotal(Empleado::calcularRetenciones);
+	}
+
+	public float calcularTotalSueldosNetos() {
+		return calcularTotal(Empleado::calcularSueldoNeto);
+	}
+
+	public float calcularTotalSueldosBrutos() {
+		return calcularTotal(Empleado::calcularSueldoBruto);
+	}
+}
