2c9c4e0c3f3efdef71a1d4ebbeba4f98842339eb
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-03-28 13:03:45 -0300

    Versión final

---
+++ src/ar/edu/unq/po2/tp2/DesgloceConceptos.java
@@ -0,0 +1,21 @@
+package ar.edu.unq.po2.tp2;
+
+public class DesgloceConceptos {
+	
+	private String concepto;
+	private float monto;
+	
+	public String getConcepto() {
+		return concepto;
+	}
+	
+	
+	public float getMonto() {
+		return monto;
+	}
+	  
+	public DesgloceConceptos (String concepto, float monto) {
+		   this.concepto = concepto ;
+		   this.monto = monto;
+	  }
+}
