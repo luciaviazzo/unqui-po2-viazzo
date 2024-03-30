4fb3e00787b0b7d0b2e0b8261cf33f961db94051
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-03-30 09:44:15 -0300

    Punto 2

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -4,23 +4,28 @@ import java.util.ArrayList;
 
 public class Counter {
 	
-	private ArrayList<Integer> numbers;
+	private ArrayList<Integer> integerNumbers;
 	
 	
 	public Counter () {
-		this.numbers = new ArrayList<Integer>();
+		this.integerNumbers = new ArrayList<Integer>();
 	}
 	
 	
+	//PUNTO 1
 	public void addNumber(int number) {
-		numbers.add(number);
+		integerNumbers.add(number);
 	}
 	
+	
+	public ArrayList<Integer> getIntegerNumbers() {
+		return integerNumbers;
+	}
 	
 	public int countMultiplesOf(int nro) {
 		int count = 0;
-		for (int number : numbers) {
-			if(number % nro == 0) {
+		for (int number : integerNumbers) {
+			if(this.isRestZero(number, nro)) {
 				count ++;
 			}
 		}
@@ -35,11 +40,55 @@ public class Counter {
 	
 	public int countUneven() {
 		int count = 0;
-		for (int number : numbers) {
-			if(!(number % 2 == 0)) {
+		for (int number : integerNumbers) {
+			if (!this.isRestZero(number, 2)) {
 				count ++;
 			}
 		}
 		return count;
-	} 
+	}
+	
+	
+	//Indica si al dividir, el resto es 0
+	public boolean isRestZero(int dividend, int divider) {
+		return (dividend % divider == 0);
+	}
+	
+	
+	
+	//PUNTO 2
+	
+	//Receibe un arreglo de numeros y devuelve el nro con mayor cantidad de digitos pares
+	public int numberMoreEvenDigits(ArrayList<Integer> numbers) {
+		int maxNumber = 0;
+		int amountDigits = 0;
+		
+		
+		for (int number : numbers) {
+			int lastAmountDigits = this.countEvenDigits(number); //Guarda la cantidad de digitos pares del numero que se esta evaluando
+			
+			//Si el numero que se esta evaluando tiene mas digitos pares que el anterior, se actualzan las variables
+			if(amountDigits < lastAmountDigits) {
+				maxNumber = number;
+				amountDigits = lastAmountDigits;
+			}
+		}
+		
+		return maxNumber;
+	}
+	
+	
+	//Devuelve la cantidad de diigitos pares de un numero
+	public int countEvenDigits(int number) {
+		int amount = 0;
+
+        while (number > 0) {
+            int digit = number % 10; //Extraer el ultimo digito
+            if (this.isRestZero(digit, 2)) { //Verificar si el dígito es par
+            	amount++; 
+            }
+            number /= 10; // Elimina el ultimo digito
+        }
+        return amount; 
+	}
 }
--- test/ar/edu/unq/po2/tp3/CounterTestCase.java
+++ test/ar/edu/unq/po2/tp3/CounterTestCase.java
@@ -1,26 +1,32 @@
 package ar.edu.unq.po2.tp3;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 public class CounterTestCase {
 	private Counter counter;
+	private ArrayList<Integer> numbers;
 	
 	@BeforeEach
 	public void setUp() throws Exception {
 
 	counter = new Counter();
 
-	counter.addNumber(10);
-	counter.addNumber(3);
-	counter.addNumber(5);
+	counter.addNumber(102);
+	counter.addNumber(321);
+	counter.addNumber(5111);
 	counter.addNumber(7);
 	counter.addNumber(9);
-	counter.addNumber(2);
+	counter.addNumber(222);
 	counter.addNumber(1);
 	counter.addNumber(5);
 	counter.addNumber(8);
-	counter.addNumber(4);
+	counter.addNumber(4);	
+	
 	}
+
 	
 	
 	@Test
@@ -39,7 +45,13 @@ public class CounterTestCase {
 	
 	@Test
 	public void testMultiplesNumbersOf3() {
-	int amount = counter.countMultiplesOf(3);
+	int amount = counter.countMultiplesOf(4);
 	assertEquals(amount, 2);
+	}
+	
+	
+	@Test
+	public void testNumberMoreEvenDigits() {
+	assertEquals(counter.numberMoreEvenDigits(counter.getIntegerNumbers()), 222);
 	}
 }
