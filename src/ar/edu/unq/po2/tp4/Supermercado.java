034a04e55f17e4d28b01b63eaf7f8b963e6e4b51
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-04-01 18:41:40 -0300

    Creo la clase supermercado y sus variables

---
+++ src/ar/edu/unq/po2/tp4/Supermercado.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.po2.tp4;
+
+import java.util.ArrayList;
+
+public class Supermercado {
+	
+	private String nombre;
+	private String direccion;
+	private ArrayList<Producto> productos;
+	
+	
+	public Supermercado(String nombre, String direccion) {
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.productos = new ArrayList<Producto>();
+	}
+	
+	
+}
