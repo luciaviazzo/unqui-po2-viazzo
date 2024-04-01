74c6580d3cb6091be1aae865aa44cbb314a10bda
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-04-01 19:02:56 -0300

    Creo la clase producto y sus variables

---
+++ src/ar/edu/unq/po2/tp4/Producto.java
@@ -0,0 +1,22 @@
+package ar.edu.unq.po2.tp4;
+
+public class Producto {
+	
+	private String nombre;
+	private float precio;
+	private boolean esPrecioCuidado;
+	
+	
+	public Producto(String nombre, float precio, boolean precioCuidado) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.esPrecioCuidado = esPrecioCuidado;
+	}	
+	
+	//Constructor sobrecargado 
+	public Producto(String nombre, float precio) {
+        this(nombre, precio, false); 
+    }
+	
+	
+}
