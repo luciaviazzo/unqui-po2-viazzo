a37984f53dfef5f500ae459ce47be15656f785f6
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-04-02 18:20:56 -0300

    Punto 2

--- src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
+++ src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
@@ -2,18 +2,25 @@ package ar.edu.unq.po2.tp4;
 
 public class ProductoPrimeraNecesidad extends Producto{
 
-	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esPrecioCuidado) {
+	private double descuento;
+	
+	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esPrecioCuidado, double descuento) {
 		super(nombre, precio, esPrecioCuidado);
-		// TODO Auto-generated constructor stub
+		this.descuento = descuento;
 	}
 	
 	
-	public double getDescuento(double porcentaje) {
-		return this.getPrecioBase() * porcentaje;
+	public double getDescuento() {
+		return descuento;
+	}
+	
+	
+	public double aplicarDescuento() {
+		return this.getPrecioBase() * this.getDescuento() / 100;
 	}
 	
 	@Override 
 	public double getPrecio() {
-		return this.getPrecioBase() - getDescuento(0.1);
+		return this.getPrecioBase() - aplicarDescuento();
 	}
 }
