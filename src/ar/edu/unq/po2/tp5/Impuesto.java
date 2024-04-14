b02557ec30864d74a62f603c07057c1483dc30ad
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-04-14 17:55:48 -0300

    Creo clase Impuesto

---
+++ src/ar/edu/unq/po2/tp5/Impuesto.java
@@ -0,0 +1,20 @@
+package ar.edu.unq.po2.tp5;
+
+public class Impuesto extends Factura{
+	
+	
+	public double tasaServicio;
+	
+
+	public Impuesto(Agencia agencia, double tasaServicio) {
+		super(agencia);
+		this.tasaServicio = tasaServicio;
+	}
+
+
+	@Override
+	public double getPrecio() {
+		return tasaServicio;
+	}
+
+}
