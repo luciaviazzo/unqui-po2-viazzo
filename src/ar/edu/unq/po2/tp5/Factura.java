239f3028a4c2549a6c401d6d0ccb0ee295e7c8a6
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-04-14 17:52:18 -0300

    Creo subclase abstracta Factura

---
+++ src/ar/edu/unq/po2/tp5/Factura.java
@@ -0,0 +1,21 @@
+package ar.edu.unq.po2.tp5;
+
+public abstract class Factura implements Pagable{
+	
+	
+	private Agencia agencia;
+	
+	
+	public Factura(Agencia agencia) {
+		this.agencia = agencia;
+	}
+
+	
+	public abstract double getPrecio();
+	
+	
+	@Override
+	public void registrar() {
+		agencia.registrarPago(this);
+	}
+}
