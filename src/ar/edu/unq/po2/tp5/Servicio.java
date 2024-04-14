4c5a5ee5ebd3286b9ade969dbf064e484f8b13dc
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-04-14 17:59:43 -0300

    Creo clase Servicio

---
+++ src/ar/edu/unq/po2/tp5/Servicio.java
@@ -0,0 +1,22 @@
+package ar.edu.unq.po2.tp5;
+
+public class Servicio extends Factura {
+	
+	
+	private double costoUnidad;
+	private int cantUnidades;
+	
+
+	public Servicio(Agencia agencia, double costoUnidad, int cantUnidades) {
+		super(agencia);
+		this.costoUnidad = costoUnidad;
+		this.cantUnidades = cantUnidades;
+	}
+	
+
+	@Override
+	public double getPrecio() {
+		return costoUnidad * cantUnidades;
+	}
+
+}
