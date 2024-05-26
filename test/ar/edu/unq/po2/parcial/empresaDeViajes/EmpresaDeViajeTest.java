514ef6b19b24391f3ae2f6aef38f4da8364e0427
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-05-26 18:57:19 -0300

    Test empresa de viajes

---
+++ test/ar/edu/unq/po2/parcial/empresaDeViajes/EmpresaDeViajeTest.java
@@ -0,0 +1,25 @@
+package ar.edu.unq.po2.parcial.empresaDeViajes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmpresaDeViajeTest {
+	
+	private Excursion excursionA;
+	private Pasaje pasajeA;
+	private Paquete paqueteA;
+	private Paquete paqueteB;
+	private Cliente cliente;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
